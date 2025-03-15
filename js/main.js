4a8e47a223f492737432b5cda99dbadbc9ee8718
Ruben Garcia <rgarcr09@gmail.com>
2025-03-14 20:03:23 -0400

    Added function to get data from api Added function to create url
    Removed overwriting onScroll and added event
    listener for scroll
    
    Removed large list or items in parameter for
    createdCard function and added card object as item
    in parameter
    
    Changed name of getFetch function to getCards
    Added variables for DOM elements in getCards

--- js/main.js
+++ js/main.js
@@ -1,29 +1,86 @@
 /*
-Try filtering set
 Limit how many to display by search
 create elements for each card entry
 have multiple pages or display for long results
 */
 
+// organize code 
+// Have a function to search cards with user input
+// have a function to populate cards
+//function to build url 
+//function to hide loader and search
+//function to show loader and search
+// change getFetch to searchCards
+
+
 // hide section-input when page loads
 document.querySelector(".section-input").style.visibility = "hidden";
 
-document.querySelector('.btn-search').addEventListener('click', getFetch)
+document.querySelector('.btn-search').addEventListener('click', getCards)
+const btnScroll = document.querySelector(".btn-scroll-up");
 
-function getFetch() {
-   
-  //make button inactive when search goes through
-  document.querySelector(".btn-search").classList.toggle("disabled");
 
-  //display searching when search btn clicked
-  document.querySelector(".notify-searching").style.visibility = "visible";
- //toggle none so animation starts when searching
-  document.querySelectorAll(".notify-searching__dot").forEach(node =>
+async function getCards() {
+
+  const btnSearch = document.querySelector(".btn-search");
+  const searchLbl = document.querySelector(".notify-searching");
+  const searchLblDots = document.querySelectorAll(".notify-searching__dot");
+  const noResultsLbl = document.querySelector(".notify-no-search");
+  const resultsContainer = document.querySelector(".cards-results");
+
+  btnSearch.classList.toggle("disabled"); //disable btn when searching
+  searchLbl.style.visibility = "visible"; //show searching lbl and animation
+  searchLblDots.forEach(node =>
+    node.classList.toggle("none"));
+  noResultsLbl.style.visibility = "hidden"; //hide no results lbl
+
+  const url = buildURL(); //get url
+  const data = await getCardData(url); //get sata from url
+
+  searchLbl.style.visibility = "hidden"; //hide searching animation after results come in
+  searchLblDots.forEach(node =>
     node.classList.toggle("none"));
 
-  //hide no results when search btn is clicked
-  document.querySelector(".notify-no-search").style.visibility = "hidden";
+  // display no results if no cards are found, enable button, and end function
+  if (!data || data.count == 0) {
+    noResultsLbl.style.visibility = "visible";
+    btnSearch.classList.toggle("disabled");
+    return;
+  }
 
+  noResultsLbl.style.visibility = "hidden"; //hide no results
+
+  // clear previous results
+  if (resultsContainer.childElementCount > 0) 
+    cardResults.innerHTML = '';
+
+  data.data.forEach(card => createCard(resultsContainer, card)); 
+  btnSearch.classList.toggle("disabled"); //enable btn-search
+
+} //end getCards
+
+
+
+// fetch data and return data
+async function getCardData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    //if theres an error send a default response 
+    console.error('Failed to fetch card data', error.message);
+    return { count: 0, data: [] };
+  }
+
+}
+
+
+// function to build url from user  input
+function buildURL() {
   const choice = document.querySelector('input').value || '';
   const option = document.querySelector('#set-select').value || "";
   const orderbyOp = document.querySelector('#order-cards-by').value;
@@ -37,7 +94,8 @@ function getFetch() {
   //https://api.pokemontcg.io/v2/cards?q=name:pikachu
 
 
-
+  //build url to search by user input
+  // reduce redundancy 
   let url = `https://api.pokemontcg.io/v2/cards?`;
   if (option.length > 0 && choice.length > 0)
     url += `q=set.id:${option} name:${choice.toLocaleLowerCase()}*`;
@@ -51,113 +109,43 @@ function getFetch() {
     url += `orderBy=${orderbyOp}`;
 
   console.log(url);
-
-
-  fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      //hide searching when data comes in
-      document.querySelector(".notify-searching").style.visibility = "hidden";
-      //toggle animation off so its not running when results come up
-      document.querySelectorAll(".notify-searching__dot").forEach(node =>
-        node.classList.toggle("none"));
-
-
-      if (data.count == 0) {
-        document.querySelector(".notify-no-search").style.visibility = "visible";
-
-      }
-      else {
-        document.querySelector(".notify-no-search").style.visibility = "hidden";
-      }
-
-
-
-      //console.log(data)
-      console.log(data);
-
-      //get container holding cards
-      let cardResults = document.querySelector(".cards-results");
-
-      //make sure I have clean slate
-      //delete all other entries
-      if (cardResults.childElementCount > 0) {
-        cardResults.innerHTML = '';
-
-      }
-
-      //loop all the cards from data
-      //create elements for card h2:name, img, ul li span: types attack rarity price 
-      //append to parent cardResults
-      data.data.forEach(card => {
-
-        //PROMISES MAKES THIS DIFFUCLT TO HANDLE PROPERTY NOT IN OBJ
-        //not all cards havecardmarket or types or attack attribute //ERROR WITH PRICES ATTRIBUTE
-
-        let cardMrkt = card.cardmarket ||= "N/A";
-        //error keeps occurring when trying to check if attribute 
-        //let cardMrktPrice = card?.cardmarket != undefined ? card.cardmarket.prices.averageSellPrice : 'N/A';
-        let cardMrktPrice = cardMrkt != 'N/A' ? card.cardmarket.prices.averageSellPrice : 'N/A';
-
-        let cardTypes = card.types ||= card.supertype ||= 'N/A';
-        let cardAttacks = card.attacks ||= ["N/A", "N/A"]
-        createCard(cardResults, card.name, card.set.series, card.set.name, card.images.small, cardTypes, cardAttacks, card.rarity, cardMrktPrice);
-
-      });
-      //after data loads toggle disable off for button search
-      document.querySelector(".btn-search").classList.toggle("disabled");
-
-    })
-
-    .catch(err => {
-      console.log(`error ${err}`)
-    });
-
+  return url;
 }
 
-//create html for card
-function createCard(containerAppend, name, series, set, img, types, attacks, rarity, cardMrkt) {
-  let cardContainr = document.createElement("section");
+
+// creater a container and add properties from card
+function createCard(container, card) {
+  console.log(card)
+  let cardContainer = document.createElement("section");
   let list = document.createElement("ul");
   let h2Name = document.createElement("h2");
-  let pkmSet = document.createElement("h2");
-  let pkmSeries = document.createElement("h3");
+  let pkmSet = document.createElement("h3");
+  let pkmSeries = document.createElement("h4");
   let pkmImg = document.createElement("img");
   let pkmTypes = document.createElement("li");
   let attack1 = document.createElement("li");
   let attack2 = document.createElement("li");
   let pkmRarity = document.createElement("li");
   let pkmAvgPrice = document.createElement("li");
-  h2Name.innerHTML = name;
-  pkmSeries.innerHTML = "Series: " + series;
-  pkmSet.innerHTML = "Set: " + set;
-  pkmImg.src = img;
-  pkmTypes.innerHTML = "Type: " + types;
-  //check if multiple attacks
-  //maybe create muliple elements for attacks or add to single element
-  if (attacks.length > 1 && typeof (attacks[0]) === 'object' && typeof (attacks[1]) === 'object') {
-    attack1.innerHTML = 'Attack1:' + (attacks[0].name);
-    attack2.innerHTML = 'Attack2: ' + (attacks[1].name);
-  }
-  else if (attacks.length === 1) {
-    attack1.innerHTML = 'Attack1:' + (attacks[0].name);
-    attack2.innerHTML = 'Attack2: N/A';
-  }
 
-  else {
-    attack1.innerHTML = 'Attack1: N/A';
-    attack2.innerHTML = 'Attack2: N/A';
-  }
+  h2Name.innerHTML = card.name;
+  pkmSeries.innerHTML = "Series: " + card.set.series;
+  pkmSet.innerHTML = "Set: " + card.set.name;
+  pkmImg.src = card.images.small;
+  pkmTypes.innerHTML = "Type: " + (card.types ? card.types : (card.supertype ? card.supertype : 'N/A'));
 
-  pkmRarity.innerHTML = "Rarity: " + rarity;
+  attack1.innerHTML = 'Attack1: ' + (card.attacks?.[0]?.name || "N/A");
+  attack2.innerHTML = 'Attack2: ' + (card.attacks?.[1]?.name || "N/A");
+
+  pkmRarity.innerHTML = "Rarity: " + (card.rarity || "N/A");
+
   //some cards do no have attribute cardmarket
-  if (cardMrkt != 'N/A')
-    cardMrkt = Number(cardMrkt).toFixed(2);
-  pkmAvgPrice.innerHTML = "AVG SellPrice: $" + cardMrkt;
+  pkmAvgPrice.innerHTML = "AVG SellPrice: $" +
+    (card.cardmarket ? Number(card.cardmarket.prices.averageSellPrice).toFixed(2) : "N/A");
 
   list.append(pkmTypes, attack1, attack2, pkmRarity, pkmAvgPrice);
-  cardContainr.append(h2Name, pkmSet, pkmSeries, pkmImg, list);
-  containerAppend.append(cardContainr);
+  cardContainer.append(h2Name, pkmSet, pkmSeries, pkmImg, list);
+  container.append(cardContainer);
 
 }
 
@@ -182,7 +170,7 @@ fetch("https://api.pokemontcg.io/v2/sets?&orderBy=series")
     });
     // show section-input when data from api fills selects in options
     document.querySelector(".section-input").style.visibility = "visible";
-    
+
     // hider loader
     document.querySelector(".search__loader").style.display = "none";
   })
@@ -190,7 +178,7 @@ fetch("https://api.pokemontcg.io/v2/sets?&orderBy=series")
     console.log(`error ${err}`)
   });
 
-  
+
 
 function searchBySeries() {
   //&orderBy=-set.releaseDate"
@@ -218,7 +206,6 @@ function getCardsBySet(setID) {
     });
 }
 
-let btnScroll = document.querySelector(".btn-scroll-up");
 
 
 // Scroll up to top of page when scroll button clicked
@@ -230,7 +217,7 @@ btnScroll.addEventListener("click", (e) => {
 
 //display or hide button when window scrolls
 // When the user scrolls down 100px from the top, show the button
-window.onscroll = function () { scrollFunction() };
+window.addEventListener("scroll", scrollFunction);
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
